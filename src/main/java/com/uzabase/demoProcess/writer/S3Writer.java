1dafc30b8b7cec04865b85535a9da472c3dcd3e6
maleenc <maleenc@gmail.com>
2024-07-16 23:00:36 +0530

    Add s3 library

---
+++ src/main/java/com/uzabase/demoProcess/writer/S3Writer.java
@@ -0,0 +1,44 @@
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.nio.file.Paths;
+
+@Service
+public class S3Writer {
+
+    private final S3Client s3Client;
+
+    public S3Writer() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create("AKIA3YNXIDJLWZOEVTNN", "EiThNAQVjmSjS1J0TbkqH9uuLGWEXCEYwh3gsMcu");
+        this.s3Client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+    public void uploadFile(String bucketName, String keyName, String filePath) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+        PutObjectResponse putObjectResponse = s3Client.putObject(putObjectRequest, Paths.get(filePath));
+
+        System.out.println("File uploaded with ETag: " + putObjectResponse.eTag());
+    }
+
+    public void uploadContent(String bucketName, String keyName, String content) {
+        s3Client.putObject(PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(keyName)
+                        .build(),
+                software.amazon.awssdk.core.sync.RequestBody.fromString(content));
+    }
+}
+
